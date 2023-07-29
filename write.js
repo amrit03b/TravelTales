055b7f03ad87b1e494261eb5e21d4688a91b7865
amrit03b <amritb9682@gmail.com>
2023-07-30 00:56:44 +0530

    okk

--- write.js
+++ write.js
@@ -1,26 +1,32 @@
-// JavaScript for write.html
+// Function to create a new journal
+function createJournal(event) {
+    event.preventDefault();
+    const journalForm = document.getElementById('journal-form');
+    const journalTitle = journalForm.elements['journalTitle'].value;
+    const journalContent = journalForm.elements['journalContent'].value;
+    const journalImage = journalForm.elements['journalImage'].files[0];
+    const publicCheck = journalForm.elements['publicCheck'].checked;
 
-// Get the form element
-const journalForm = document.getElementById('journalForm');
+    // Create a new card to display the journal details
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <img src="${journalImage ? URL.createObjectURL(journalImage) : 'placeholder.jpg'}" class="card-img-top" alt="Journal Image">
+        <div class="card-body">
+            <h5 class="card-title">${journalTitle}</h5>
+            <p class="card-text">${journalContent}</p>
+            <p class="card-text">${publicCheck ? 'Public' : 'Private'}</p>
+        </div>
+    `;
 
-// Add a submit event listener to the form
-journalForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission to server for now
-    
-    // Get form data
-    const formData = new FormData(journalForm);
-    const title = formData.get('title');
-    const content = formData.get('content');
-    const image = formData.get('image');
-    const publishOption = formData.get('publishOption');
+    // Add the new card to the published journals container
+    const publishedJournals = document.getElementById('publishedJournals');
+    publishedJournals.appendChild(card);
 
-    // Perform any other actions with form data, such as saving to a database or showing a confirmation message
-    // For now, we will just log the data to the console
-    console.log('Title:', title);
-    console.log('Content:', content);
-    console.log('Image:', image);
-    console.log('Publish Option:', publishOption);
-
-    // Reset the form
+    // Reset the form fields
     journalForm.reset();
-});
+}
+
+// Event listener for the form submission
+const journalForm = document.getElementById('journal-form');
+journalForm.addEventListener('submit', createJournal);
